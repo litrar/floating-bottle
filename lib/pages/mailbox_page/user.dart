0b86699546e95cf4bb5414076a700bc091f9fdd8
M <110306087@nccu.edu.tw>
2023-05-20 11:39:54 +0800

    add user page

---
+++ lib/pages/mailbox_page/user.dart
@@ -0,0 +1,11 @@
+import 'letter.dart';
+
+class User{
+  String picture;
+  String name;
+  Letter letter;
+  String date;
+  String time;
+
+  User(this.picture,this.name,this.letter,this.date,this.time);
+}
