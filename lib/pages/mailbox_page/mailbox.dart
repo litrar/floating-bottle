02f74b3a7cda4276ba6a43cc25d603ad7d7378b1
M <110306087@nccu.edu.tw>
2023-05-20 11:39:20 +0800

    update mailbox page

--- lib/pages/mailbox_page/mailbox.dart
+++ lib/pages/mailbox_page/mailbox.dart
@@ -1,3 +1,6 @@
+import 'package:floating_bottle/pages/mailbox_page/friend.dart';
+import 'package:floating_bottle/pages/mailbox_page/letter.dart';
+import 'package:floating_bottle/pages/mailbox_page/user.dart';
 import 'package:floating_bottle/pages/subpage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,6 +9,7 @@ import 'package:go_router/go_router.dart';
 import '../components/bottom_bar.dart';
 import '../theme/color_theme.dart';
 import '../theme/theme_bloc.dart';
+import 'letter_content.dart';
 import 'mail_route.dart';
 
 
@@ -26,27 +30,152 @@ class MailboxSubPage implements SubPage {
 }
 
 class MailBoxPage extends StatelessWidget {
-  MailBoxPage({super.key}) {}
+  MailBoxPage({Key? key}) : super(key: key);
+
+  final Friend friend = Friend([
+    User(
+      "assetsfolder/avatar.jpg",
+      "Ann",
+      Letter("Ann", "assetsfolder/avatar.jpg", "It was great to hear from you. I'm writing to you..."),
+      "2023/5/19",
+      "20:45",
+    ),
+    User(
+      "assetsfolder/avatar.jpg",
+      "Pink",
+      Letter("Pink", "assetsfolder/avatar.jpg", "Happy Wednesday! I hope this email finds you..."),
+      "2023/5/19",
+      "20:45",
+    ),
+    User(
+      "assetsfolder/avatar.jpg",
+      "HiChew",
+      Letter("HiChew", "assetsfolder/avatar.jpg", "Thank you for your last email. Sorry for the late..."),
+      "2023/5/19",
+      "20:45",
+    ),
+    User(
+      "assetsfolder/avatar.jpg",
+      "Emma Lin",
+      Letter("Emma Lin", "assetsfolder/avatar.jpg", "April Fool's pranks are way too predictable. I'll..."),
+      "2023/5/19",
+      "20:45",
+    ),
+  ]);
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ThemeCubit, ColorTheme>(builder: (context, state) {
-      return Scaffold(
-        bottomNavigationBar: BottomBar(SubPage.CONTACT),
-        body: Stack(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assetsfolder/personal_background.jpg"),
-                  fit: BoxFit.cover,
+    return BlocBuilder<ThemeCubit, ColorTheme>(
+      builder: (context, state) {
+        return Scaffold(
+          bottomNavigationBar: BottomBar(SubPage.CONTACT),
+          body: Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assetsfolder/personal_background.jpg"),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            Text('this is mailbox page', style: TextStyle(fontSize: 20.sp),)
-          ],
-        ),
-      );
-    });
+              Container(
+                margin: EdgeInsets.only(top: 100,bottom: 200),
+                child: Column(
+                  children: [
+                    Text(
+                      'New Letters',
+                      style: TextStyle(
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.black,
+                      endIndent: 0,
+                      indent: 0,
+                      thickness: 2,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 105),
+                child: ListView(
+                  children: friend.friends.map((fp) {
+                    return Container(
+                      margin: EdgeInsets.only(top: 0,bottom: 0,right: 10,left: 10),
+                      child: Column(
+                        children: [
+                          InkWell(
+                            child: Row(
+                              children: [
+                                ClipRRect(
+                                    borderRadius: BorderRadius.circular(500),
+                                    child: Image.asset("${fp.picture}",height: 70,width: 70,)
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "  ${fp.name}",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 20,
+                                          ),
+                                        ),
+                                        Column(
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Text("    ${fp.date}"),
+                                                Text("  ${fp.time}")
+                                              ],
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text("  ${fp.letter.content}",style: TextStyle(
+                                            color: Colors.grey.shade700
+                                        ),)
+                                      ],
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => LetterContent(name: "${fp.name}"),
+                                ),
+                              );
+                            },
+                          ),
+                          Divider(
+                            color: Colors.black,
+                            thickness: 2,
+                            indent: 0,
+                            endIndent: 0,
+                          ),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
   }
 }
+
+
