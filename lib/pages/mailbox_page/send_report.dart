828d2605aef12b58d126e2e724aae906af60cf36
M <110306087@nccu.edu.tw>
2023-05-22 17:03:48 +0800

    add send report page

---
+++ lib/pages/mailbox_page/send_report.dart
@@ -0,0 +1,50 @@
+import 'package:floating_bottle/pages/mailbox_page/report.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SendReport extends StatefulWidget {
+  const SendReport({Key? key}) : super(key: key);
+
+  @override
+  State<SendReport> createState() => _SendReportState();
+}
+class _SendReportState extends State<SendReport> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          const Report(),
+          Container(
+            margin: const EdgeInsets.all(0),
+            color: Colors.white60,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 340.h,left: 85.w,right: 70.w,bottom: 270.h),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.black54,
+            ),
+            child: SizedBox(
+              width: 200.w,
+              height: 50.h,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
+                    )
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("Report", style: TextStyle(
+                    fontSize: 22.sp,color: Colors.white,fontFamily: 'Bellota-Regular'
+                ),),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
