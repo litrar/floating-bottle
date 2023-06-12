6f9e5958b51369bae05549aa22a030c9763494c0
M <110306087@nccu.edu.tw>
2023-06-12 16:03:17 +0800

    update

--- lib/api/letter/letter_api_imp.dart
+++ lib/api/letter/letter_api_imp.dart
@@ -8,13 +8,26 @@ class _LetterApi implements LetterApi {
   _LetterApi(this._dio);
 
   @override
-  Future<HttpRes<LetterGot>> getLetter(int matcher, int matched) async {
+  Future<HttpRes<List<LetterGot>>> getLetter(int matcherId, int matchedAccountId) async {
     var req =
     //這邊要再加matcher, matched的變數到網址裡面
-        await _dio.get("https://ecc3-111-241-177-116.ngrok-free.app/api/Letter");
-    print(req.data);
-    return HttpRes<LetterGot>.fromJson(req.data,
-        code: req.statusCode, dataDecodeFunc: LetterGot.fromJson);
+        await _dio.get("https://ecc3-111-241-177-116.ngrok-free.app/api/Letter?matcher=$matcherId&matched=$matchedAccountId");
+    if (req.statusCode == 200) {
+      final List<dynamic> jsonData = req.data;
+      final letters = jsonData
+          .map((json) => LetterGot.fromJson(json))
+          .toList()
+          .cast<LetterGot>();
+      return HttpRes<List<LetterGot>>(
+        code: req.statusCode,
+        isSuccess: true,
+        message: 'Success',
+        data: letters,
+      );
+    } else {
+      throw Exception('Failed to fetch letters');
+    }
+
   }
 
   @override
