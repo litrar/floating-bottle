28dcac2bb1709c67fde93242f57df8c21ef6704b
M <110306087@nccu.edu.tw>
2023-06-12 16:02:49 +0800

    update

--- lib/api/letter/letter_api.dart
+++ lib/api/letter/letter_api.dart
@@ -3,7 +3,7 @@ part of '../letter.dart';
 abstract class LetterApi {
    factory LetterApi(Dio dio) => _LetterApi(dio);
 
-  Future<HttpRes<LetterGot>> getLetter(int matcher, int matched);
+  Future<HttpRes<List<LetterGot>>> getLetter(int matcherId, int matchedAccountId);
   Future<bool> sendLetter(LetterSent createData);
   Future<HttpRes> deleteLetter(int letterId);
 }
