838b86fe1d82f6a04a6cd67d25fed639e2fdcd47
M <110306087@nccu.edu.tw>
2023-06-12 16:11:37 +0800

    update

--- lib/pages/match_page/match_route.dart
+++ lib/pages/match_page/match_route.dart
@@ -1,9 +1,9 @@
+import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:go_router/go_router.dart';
 import 'match.dart';
 
-final match_route = GoRoute(
-    path: "/match",
-    builder: (context, state) => MatchPage(),
-    routes: [
-      ]
+final match_route = GetPage(
+      name: "/match",
+      page: () => MatchPage(),
+      children: [],
 );
