f5838d50c9d63a865bf1bb6c85b30dce1c9e6671
M <110306087@nccu.edu.tw>
2023-06-12 16:04:16 +0800

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,95 @@
+// import 'package:dio/dio.dart';
+// import 'package:floating_bottle/api/match/models/filter_fillout_data.dart';
+// import 'package:floating_bottle/pages/authentication/login.dart';
+// import 'package:floating_bottle/pages/components/error_page.dart';
+// import 'package:floating_bottle/pages/subpage.dart';
+// import 'package:floating_bottle/pages/theme/theme_bloc.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:go_router/go_router.dart';
+//
+// import 'api/letter.dart';
+// import 'api/match.dart';
+//
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   final Dio _dio = Dio(BaseOptions(
+//       connectTimeout: Duration(milliseconds: 10000),
+//       receiveTimeout: Duration(milliseconds: 10000),
+//       sendTimeout: Duration(milliseconds: 10000),
+//       validateStatus: (_) => true));
+//   late GoRouter _goRouter;
+//   MyApp({super.key}) {
+//     _goRouter = GoRouter(
+//       // navigatorKey: NavigatorProvider.navigatorKey,
+//         initialLocation: Login.route.path,
+//         routes: [
+//           Login.route,
+//           for (var page in SubPage.PAGES) page.route,
+//         ],
+//         // refreshListenable: _authNotifier,
+//         // redirect: _guard,
+//         errorBuilder: ((context, state) =>
+//             ErrorPage("uncaught error: ${state.error}")));
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScreenUtilInit(
+//       designSize: const Size(360, 690),
+//       minTextAdapt: true,
+//       splitScreenMode: true,
+//       builder: (context, child) {
+//         return MultiBlocProvider(
+//             providers: [BlocProvider(create: (_) => ThemeCubit())],
+//             // child: _app()
+//             child: MultiRepositoryProvider(providers: [
+//               RepositoryProvider(create: (_) => LetterApi(_dio)),
+//               RepositoryProvider(create: (_) => MatchApi(_dio)),
+//               RepositoryProvider(
+//                 create: (_) => FilterFillOutData(
+//                     college: '',
+//                     department: '',
+//                     sex: '',
+//                     city: '',
+//                     languages: [],
+//                     personalties: [],
+//                     interests: []),
+//               ),
+//             ],
+//                 child: _app()));
+//       },
+//     );
+//   }
+//
+//   Widget _app() {
+//     return MaterialApp.router(
+//         routerConfig: _goRouter,
+//         theme: ThemeData(primarySwatch: Colors.orange),
+//         //navigatorKey: NavigatorProvider.navigatorKey,
+//         debugShowCheckedModeBanner: false);
+//   }
+// }
 import 'package:dio/dio.dart';
 import 'package:floating_bottle/api/match/models/filter_fillout_data.dart';
 import 'package:floating_bottle/pages/authentication/login.dart';
 import 'package:floating_bottle/pages/components/error_page.dart';
+import 'package:floating_bottle/pages/personal_page/personal_page.dart';
 import 'package:floating_bottle/pages/subpage.dart';
 import 'package:floating_bottle/pages/theme/theme_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
+import 'package:get/get.dart';
+
 
 import 'api/letter.dart';
 import 'api/match.dart';
+import 'controllers/account_detail_controller.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,25 +97,29 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  AccountDetailController accountDetailController = Get.put(AccountDetailController());
   final Dio _dio = Dio(BaseOptions(
-      connectTimeout: 10000,
-      receiveTimeout: 10000,
-      sendTimeout: 10000,
+      connectTimeout: Duration(milliseconds: 10000),
+      receiveTimeout: Duration(milliseconds: 10000),
+      sendTimeout: Duration(milliseconds: 10000),
       validateStatus: (_) => true));
-  late GoRouter _goRouter;
-  MyApp({super.key}) {
-    _goRouter = GoRouter(
-        // navigatorKey: NavigatorProvider.navigatorKey,
-        initialLocation: Login.route.path,
-        routes: [
-          Login.route,
-          for (var page in SubPage.PAGES) page.route,
-        ],
-        // refreshListenable: _authNotifier,
-        // redirect: _guard,
-        errorBuilder: ((context, state) =>
-            ErrorPage("uncaught error: ${state.error}")));
+  late GetMaterialApp _getMaterialApp;
+
+  MyApp({Key? key}) {
+    _getMaterialApp = GetMaterialApp(
+      initialRoute: Login.route.name,
+      getPages: [
+        GetPage(name: Login.route.name, page: () => Login()),
+        for (var page in SubPage.PAGES)
+          GetPage(name: page.route.name, page: () => PersonalPage(accountDetailController.nameController.text)),
+      ],
+      unknownRoute: GetPage(
+        name: '/error',
+        page: () => ErrorPage("uncaught error"),
+      ),
+    );
   }
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -45,32 +128,31 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) {
         return MultiBlocProvider(
-            providers: [BlocProvider(create: (_) => ThemeCubit())],
-            // child: _app()
-            child: MultiRepositoryProvider(providers: [
+          providers: [BlocProvider(create: (_) => ThemeCubit())],
+          child: MultiRepositoryProvider(
+            providers: [
               RepositoryProvider(create: (_) => LetterApi(_dio)),
               RepositoryProvider(create: (_) => MatchApi(_dio)),
               RepositoryProvider(
-                  create: (_) => FilterFillOutData(
-                      college: '',
-                      department: '',
-                      sex: '',
-                      city: '',
-                      languages: [],
-                      personalties: [],
-                      interests: []),
-                      ),
-            ], 
-            child: _app()));
+                create: (_) => FilterFillOutData(
+                  college: '',
+                  department: '',
+                  sex: '',
+                  city: '',
+                  languages: [],
+                  personalties: [],
+                  interests: [],
+                ),
+              ),
+            ],
+            child: _app(),
+          ),
+        );
       },
     );
   }
 
   Widget _app() {
-    return MaterialApp.router(
-        routerConfig: _goRouter,
-        theme: ThemeData(primarySwatch: Colors.orange),
-        //navigatorKey: NavigatorProvider.navigatorKey,
-        debugShowCheckedModeBanner: false);
+    return _getMaterialApp;
   }
 }
