97cd60a7097506a7712886066bb8bb51e86abf7d
M <110306087@nccu.edu.tw>
2023-06-12 16:09:23 +0800

    update

--- lib/pages/contact_page/c_route.dart
+++ lib/pages/contact_page/c_route.dart
@@ -1,20 +1,23 @@
 import 'package:floating_bottle/pages/contact_page/contact_history.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:go_router/go_router.dart';
 import '../write_letter.dart';
 import 'contact.dart';
 
-final c_route = GoRoute(
-    path: "/contact",
-    builder: (context, state) => ContactPage(),
-    routes: [
-      GoRoute(
-          path: "history",
-          // path: "history/:user"
-          builder: (context, state) => ContactHistory(),
-          // builder: (context, state) => ContactHistory(state.params["user"] ?? ""),
-        
-          routes: [
-            GoRoute(path:"write", builder: (context, state) => WriteLetter()),
-            // GoRoute(path:"letter", builder: (context, state) => LetterContent()),
-          ]),
-    ]);
+final c_route = GetPage(
+  name: "/contact",
+  page: () => ContactPage(),
+  children: [
+    GetPage(
+      name: "/contact/history",
+      page: () => ContactHistory(),
+      children: [
+        GetPage(
+          name: "/contact/history/write",
+          page: () => WriteLetter(),
+        ),
+      ],
+    ),
+  ],
+);
+
